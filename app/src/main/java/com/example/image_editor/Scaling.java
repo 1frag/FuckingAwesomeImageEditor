d1a67efd626e7e147a9b994932b6d308ae073399
somnoynadno <somnoynadno@yandex.ru>
2019-04-28 20:32:34 +0700

    Save button to scaling activity added

--- app/src/main/java/com/example/image_editor/Scaling.java
+++ app/src/main/java/com/example/image_editor/Scaling.java
@@ -3,25 +3,34 @@ package com.example.image_editor;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Calendar;
 
 public class Scaling extends AppCompatActivity {
 
     private ImageView imageView;
     private Bitmap bitmap;
+    private Bitmap bufferedBitmap;
     private String path;
+
+    private String IMAGE_DIRECTORY = "/demonuts";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +54,12 @@ public class Scaling extends AppCompatActivity {
         this.imageView.setImageBitmap(this.bitmap);
 
         configApplyButton();
-
+        configSaveButton();
     }
 
     private void configApplyButton(){
-        Button filterButton = (Button) findViewById(R.id.apply_button);
-        filterButton.setOnClickListener(new View.OnClickListener() {
+        Button applyButton = (Button) findViewById(R.id.apply_button);
+        applyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText coef_width =  (EditText) findViewById(R.id.coef_width);
@@ -59,12 +68,24 @@ public class Scaling extends AppCompatActivity {
                 float h = Float.parseFloat(coef_height.getText().toString());
 
                 resizeImage(w, h);
+                // TODO: catch exceptions here
             }
         });
 
     }
 
-    // TODO: add configSaveButton here
+    private void configSaveButton(){
+        Button saveButton = (Button) findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String path = saveImage(bufferedBitmap);
+                Toast.makeText(Scaling.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+                finish();
+                return;
+            }
+        });
+    }
 
     private void resizeImage(float coef_w, float coef_h){
         Bitmap bmOriginal = this.bitmap;
@@ -74,8 +95,37 @@ public class Scaling extends AppCompatActivity {
         int newWidth = Math.round(width*coef_w);
         int newHeight = Math.round(height*coef_h);
 
-        Bitmap newBitmap = Bitmap.createScaledBitmap(bmOriginal, newWidth,
+        this.bufferedBitmap = Bitmap.createScaledBitmap(bmOriginal, newWidth,
                 newHeight, false);
-        this.imageView.setImageBitmap(newBitmap);
+        this.imageView.setImageBitmap(this.bufferedBitmap);
+    }
+
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
     }
 }
