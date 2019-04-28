a047b79c1eac07261cb2b630723e09eb2832d680
somnoynadno <somnoynadno@yandex.ru>
2019-04-28 16:37:06 +0700

    Button for scaling activity

--- app/src/main/java/com/example/image_editor/Filters.java
+++ app/src/main/java/com/example/image_editor/Filters.java
@@ -1,11 +1,13 @@
 package com.example.image_editor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Filters extends AppCompatActivity {
 
@@ -24,6 +26,18 @@ public class Filters extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        configScalingButton();
     }
 
+    private void configScalingButton(){
+        Button filterButton = (Button) findViewById(R.id.open_scaling);
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Filters.this, Scaling.class));
+            }
+        });
+
+    }
 }
