72bb9840a11d7ab2527f3d9fe5b203a027a6bb0d
somnoynadno <somnoynadno@yandex.ru>
2019-04-28 20:02:17 +0700

    Simple scaling added

--- app/src/main/java/com/example/image_editor/Scaling.java
+++ app/src/main/java/com/example/image_editor/Scaling.java
@@ -2,14 +2,15 @@ package com.example.image_editor;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -32,7 +33,7 @@ public class Scaling extends AppCompatActivity {
         Intent intent = getIntent();
         this.path = intent.getStringExtra("Image");
 
-        imageView = (ImageView) findViewById(R.id.imageScaling);
+        this.imageView = (ImageView) findViewById(R.id.imageScaling);
         try{
             this.bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.fromFile(new File(this.path)));
         }
@@ -41,17 +42,40 @@ public class Scaling extends AppCompatActivity {
             e.printStackTrace();
             Toast.makeText(Scaling.this, "Failed!", Toast.LENGTH_SHORT).show();
         }
+        this.imageView.setImageBitmap(this.bitmap);
 
-        imageView.setImageBitmap(this.bitmap);
+        configApplyButton();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    private void configApplyButton(){
+        Button filterButton = (Button) findViewById(R.id.apply_button);
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText coef_width =  (EditText) findViewById(R.id.coef_width);
+                EditText coef_height =  (EditText) findViewById(R.id.coef_height);
+                int w = Integer.parseInt(coef_width.getText().toString());
+                int h = Integer.parseInt(coef_height.getText().toString());
+
+                resizeImage(w, h);
+            }
+        });
+
+    }
+
+    // TODO: add configSaveButton here
+
+    private void resizeImage(int coef_w, int coef_h){
+        Bitmap bmOriginal = this.bitmap;
+        int width = bmOriginal.getWidth();
+        int height = bmOriginal.getHeight();
+
+        int newWidth = width*coef_w;
+        int newHeight = height*coef_h;
+
+        Bitmap newBitmap = Bitmap.createScaledBitmap(bmOriginal, newWidth,
+                newHeight, false);
+        this.imageView.setImageBitmap(newBitmap);
+    }
 }
