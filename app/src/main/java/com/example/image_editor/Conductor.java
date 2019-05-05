6008ae35d2d2159cf2204989914e7e75ee86d1c6
Aleksey Piskunov <piskunov.alesha@gmail.com>
2019-05-06 02:20:31 +0700

    02:20

--- app/src/main/java/com/example/image_editor/Conductor.java
+++ app/src/main/java/com/example/image_editor/Conductor.java
@@ -1,13 +1,9 @@
 package com.example.image_editor;
-
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
-public class Conductor {
+class Conductor {
 
     private Button btn4;
 
