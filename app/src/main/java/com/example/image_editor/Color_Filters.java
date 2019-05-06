d14898964254c7509d1dbe3c945c80df8ba1fa73
somnoynadno <somnoynadno@yandex.ru>
2019-05-06 23:01:09 +0700

    Color filters added, need dialog alert!

--- app/src/main/java/com/example/image_editor/Color_Filters.java
+++ app/src/main/java/com/example/image_editor/Color_Filters.java
@@ -1,10 +1,12 @@
 package com.example.image_editor;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -24,46 +26,33 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 
-public class Color_Filters extends AppCompatActivity {
+import static android.app.PendingIntent.getActivity;
 
-    private ImageView imageView;
+public class Color_Filters extends Conductor {
+
     private Bitmap bitmap;
     private Bitmap bufferedBitmap;
-    private String path;
+
+    private Button selectFilter;
 
     private String IMAGE_DIRECTORY = "/demonuts";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_color__filters);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    private ImageView imageView;
+    private DesignerSingleton managerDesign;
 
-
-        Intent intent = getIntent();
-        this.path = intent.getStringExtra("Image");
-
-        this.imageView = (ImageView) findViewById(R.id.imageColorFilters);
-        try{
-            this.bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.fromFile(new File(this.path)));
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(Color_Filters.this, "Failed!", Toast.LENGTH_SHORT).show();
-        }
-        this.imageView.setImageBitmap(this.bitmap);
-
-
-         configSelectFilterButton();
-         configSaveButton();
+    Color_Filters(DesignerSingleton managerDesign) {
+        super(managerDesign.btn4);
+        this.managerDesign = managerDesign;
+        this.selectFilter = managerDesign.btn1;
+        this.imageView = managerDesign.imageView;
     }
 
-    private void configSelectFilterButton(){
-        Button selectFilterButton = (Button) findViewById(R.id.filter_selector);
-        selectFilterButton.setOnClickListener(new View.OnClickListener() {
+    private void configSelectFilterButton(Button button) {
+        button.setText("select filter");
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showFilterDialog();
@@ -71,27 +60,23 @@ public class Color_Filters extends AppCompatActivity {
         });
     }
 
-    private void configSaveButton(){
-        Button saveButton = (Button) findViewById(R.id.save_button_scaling);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String path = saveImage(bufferedBitmap);
-                Toast.makeText(Color_Filters.this, "Image Saved!", Toast.LENGTH_SHORT).show();
-                finish();
-                return;
-            }
-        });
+    void touchToolbar() {
+        super.touchToolbar();
+        managerDesign.btn4.setText("Do algo");
+        configSelectFilterButton(managerDesign.btn1);
+
+        bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        imageView.setImageBitmap(bitmap);
     }
 
     private void showFilterDialog() {
-        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
-        pictureDialog.setTitle("Select color filter");
+        AlertDialog.Builder filterDialog = new AlertDialog.Builder();
+        filterDialog.setTitle("Select color filter");
         String[] pictureDialogItems = {
                 "Movie",
                 "Blur",
                 "Black and white"};
-        pictureDialog.setItems(pictureDialogItems,
+        filterDialog.setItems(pictureDialogItems,
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -100,303 +85,17 @@ public class Color_Filters extends AppCompatActivity {
                         filterAsync.execute(which);
                     }
                 });
-        pictureDialog.show();
-    }
-
-    public String saveImage(Bitmap myBitmap) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
-        File wallpaperDirectory = new File(
-                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
-        // have the object build the directory structure, if needed.
-        if (!wallpaperDirectory.exists()) {
-            wallpaperDirectory.mkdirs();
-        }
-
-        try {
-            File f = new File(wallpaperDirectory, Calendar.getInstance()
-                    .getTimeInMillis() + ".jpg");
-            f.createNewFile();
-            FileOutputStream fo = new FileOutputStream(f);
-            fo.write(bytes.toByteArray());
-            MediaScannerConnection.scanFile(this,
-                    new String[]{f.getPath()},
-                    new String[]{"image/jpeg"}, null);
-            fo.close();
-            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
-
-            return f.getAbsolutePath();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        return "";
-    }
-
-    public Bitmap movieFilter(Bitmap sentBitmap){
-        Bitmap bufBitmap = Bitmap.createBitmap(sentBitmap.getWidth(),sentBitmap.getHeight(), sentBitmap.getConfig());
-
-        for(int i=0; i<sentBitmap.getWidth(); i++){
-            for(int j=0; j<sentBitmap.getHeight(); j++){
-                int p = sentBitmap.getPixel(i, j);
-                int r = Color.red(p);
-                int g = Color.green(p);
-                int b = Color.blue(p);
-
-                b = (int) (b*0.8);
-                g = (int) (g*0.9);
-                bufBitmap.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
-            }
-        }
-
-        return bufBitmap;
-
-    }
-
-    public Bitmap fastBlur(Bitmap sentBitmap) {
-        // params for filter
-        float scale = 1;
-        int radius = 5;
-
-        int width = Math.round(sentBitmap.getWidth() * scale);
-        int height = Math.round(sentBitmap.getHeight() * scale);
-        sentBitmap = Bitmap.createScaledBitmap(sentBitmap, width, height, false);
-
-        Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
-
-        if (radius < 1) {
-            return (null);
-        }
-
-        int w = bitmap.getWidth();
-        int h = bitmap.getHeight();
-
-        int[] pix = new int[w * h];
-        Log.e("pix", w + " " + h + " " + pix.length);
-        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
-
-        int wm = w - 1;
-        int hm = h - 1;
-        int wh = w * h;
-        int div = radius + radius + 1;
-
-        int r[] = new int[wh];
-        int g[] = new int[wh];
-        int b[] = new int[wh];
-        int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
-        int vmin[] = new int[Math.max(w, h)];
-
-        int divsum = (div + 1) >> 1;
-        divsum *= divsum;
-        int dv[] = new int[256 * divsum];
-        for (i = 0; i < 256 * divsum; i++) {
-            dv[i] = (i / divsum);
-        }
-
-        yw = yi = 0;
-
-        int[][] stack = new int[div][3];
-        int stackpointer;
-        int stackstart;
-        int[] sir;
-        int rbs;
-        int r1 = radius + 1;
-        int routsum, goutsum, boutsum;
-        int rinsum, ginsum, binsum;
-
-        for (y = 0; y < h; y++) {
-            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
-            for (i = -radius; i <= radius; i++) {
-                p = pix[yi + Math.min(wm, Math.max(i, 0))];
-                sir = stack[i + radius];
-                sir[0] = (p & 0xff0000) >> 16;
-                sir[1] = (p & 0x00ff00) >> 8;
-                sir[2] = (p & 0x0000ff);
-                rbs = r1 - Math.abs(i);
-                rsum += sir[0] * rbs;
-                gsum += sir[1] * rbs;
-                bsum += sir[2] * rbs;
-                if (i > 0) {
-                    rinsum += sir[0];
-                    ginsum += sir[1];
-                    binsum += sir[2];
-                } else {
-                    routsum += sir[0];
-                    goutsum += sir[1];
-                    boutsum += sir[2];
-                }
-            }
-            stackpointer = radius;
-
-            for (x = 0; x < w; x++) {
-
-                r[yi] = dv[rsum];
-                g[yi] = dv[gsum];
-                b[yi] = dv[bsum];
-
-                rsum -= routsum;
-                gsum -= goutsum;
-                bsum -= boutsum;
-
-                stackstart = stackpointer - radius + div;
-                sir = stack[stackstart % div];
-
-                routsum -= sir[0];
-                goutsum -= sir[1];
-                boutsum -= sir[2];
-
-                if (y == 0) {
-                    vmin[x] = Math.min(x + radius + 1, wm);
-                }
-                p = pix[yw + vmin[x]];
-
-                sir[0] = (p & 0xff0000) >> 16;
-                sir[1] = (p & 0x00ff00) >> 8;
-                sir[2] = (p & 0x0000ff);
-
-                rinsum += sir[0];
-                ginsum += sir[1];
-                binsum += sir[2];
-
-                rsum += rinsum;
-                gsum += ginsum;
-                bsum += binsum;
-
-                stackpointer = (stackpointer + 1) % div;
-                sir = stack[(stackpointer) % div];
-
-                routsum += sir[0];
-                goutsum += sir[1];
-                boutsum += sir[2];
-
-                rinsum -= sir[0];
-                ginsum -= sir[1];
-                binsum -= sir[2];
-
-                yi++;
-            }
-            yw += w;
-        }
-        for (x = 0; x < w; x++) {
-            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
-            yp = -radius * w;
-            for (i = -radius; i <= radius; i++) {
-                yi = Math.max(0, yp) + x;
-
-                sir = stack[i + radius];
-
-                sir[0] = r[yi];
-                sir[1] = g[yi];
-                sir[2] = b[yi];
-
-                rbs = r1 - Math.abs(i);
-
-                rsum += r[yi] * rbs;
-                gsum += g[yi] * rbs;
-                bsum += b[yi] * rbs;
-
-                if (i > 0) {
-                    rinsum += sir[0];
-                    ginsum += sir[1];
-                    binsum += sir[2];
-                } else {
-                    routsum += sir[0];
-                    goutsum += sir[1];
-                    boutsum += sir[2];
-                }
-
-                if (i < hm) {
-                    yp += w;
-                }
-            }
-            yi = x;
-            stackpointer = radius;
-            for (y = 0; y < h; y++) {
-                // Preserve alpha channel: ( 0xff000000 & pix[yi] )
-                pix[yi] = ( 0xff000000 & pix[yi] ) | ( dv[rsum] << 16 ) | ( dv[gsum] << 8 ) | dv[bsum];
-
-                rsum -= routsum;
-                gsum -= goutsum;
-                bsum -= boutsum;
-
-                stackstart = stackpointer - radius + div;
-                sir = stack[stackstart % div];
-
-                routsum -= sir[0];
-                goutsum -= sir[1];
-                boutsum -= sir[2];
-
-                if (x == 0) {
-                    vmin[y] = Math.min(y + r1, hm) * w;
-                }
-                p = x + vmin[y];
-
-                sir[0] = r[p];
-                sir[1] = g[p];
-                sir[2] = b[p];
-
-                rinsum += sir[0];
-                ginsum += sir[1];
-                binsum += sir[2];
-
-                rsum += rinsum;
-                gsum += ginsum;
-                bsum += binsum;
-
-                stackpointer = (stackpointer + 1) % div;
-                sir = stack[stackpointer];
-
-                routsum += sir[0];
-                goutsum += sir[1];
-                boutsum += sir[2];
-
-                rinsum -= sir[0];
-                ginsum -= sir[1];
-                binsum -= sir[2];
-
-                yi += w;
-            }
-        }
-
-        Log.e("pix", w + " " + h + " " + pix.length);
-        bitmap.setPixels(pix, 0, w, 0, 0, w, h);
-
-        return (bitmap);
-    }
-
-    public Bitmap createGrayscale(Bitmap src) {
-        int width = src.getWidth();
-        int height = src.getHeight();
-        // create output bitmap
-        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
-        // color information
-        int A, R, G, B;
-        int pixel;
-
-        // scan through all pixels
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
-                // get pixel color
-                pixel = src.getPixel(x, y);
-                A = Color.alpha(pixel);
-                R = Color.red(pixel);
-                G = Color.green(pixel);
-                B = Color.blue(pixel);
-                int gray = (int) (0.2989 * R + 0.5870 * G + 0.1140 * B);
-                // set new pixel color to output bitmap
-                bmOut.setPixel(x, y, Color.argb(A, gray, gray, gray));
-            }
-        }
-        return bmOut;
+        filterDialog.show();
     }
 
     private class AsyncTaskFilters extends AsyncTask <Integer, Void, Bitmap> {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            Toast.makeText(getApplicationContext(), "New thread created", Toast.LENGTH_SHORT).show();
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            Toast.makeText(getApplicationContext(), "New thread created", Toast.LENGTH_SHORT).show();
 
-        }
+//        }
 
         @Override
         protected Bitmap doInBackground(Integer... which) {
@@ -404,17 +103,17 @@ public class Color_Filters extends AppCompatActivity {
             Bitmap result = null;
             switch (a) {
                 case 0:
-                    result = movieFilter(bitmap);
+                    result = ColorFIltersCollection.movieFilter(bitmap);
                     break;
                 case 1:
-                    result = fastBlur(bitmap);
+                    result = ColorFIltersCollection.fastBlur(bitmap);
                     break;
                 case 2:
-                    result = createGrayscale(bitmap);
+                    result = ColorFIltersCollection.createGrayScale(bitmap);
                     break;
             }
             if (result == null){
-                Toast.makeText(getApplicationContext(), "This wasn't supposed to happen.", Toast.LENGTH_LONG).show();
+//                Toast.makeText(getApplicationContext(), "This wasn't supposed to happen.", Toast.LENGTH_LONG).show();
                 return bitmap;
             }
             return result;
@@ -425,7 +124,7 @@ public class Color_Filters extends AppCompatActivity {
             super.onPostExecute(result);
             bufferedBitmap = result;
             imageView.setImageBitmap(result);
-            Toast.makeText(getApplicationContext(), "SO GOOD", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(getApplicationContext(), "SO GOOD", Toast.LENGTH_SHORT).show();
         }
     }
 
