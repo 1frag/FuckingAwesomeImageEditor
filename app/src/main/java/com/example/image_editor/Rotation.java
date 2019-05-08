fb61ea955a7e91718e4b42976db94c702bcb2c32
somnoynadno <somnoynadno@yandex.ru>
2019-05-08 21:48:51 +0700

    Rotation added

--- app/src/main/java/com/example/image_editor/Rotation.java
+++ app/src/main/java/com/example/image_editor/Rotation.java
@@ -10,6 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Rotation extends Conductor {
@@ -18,9 +21,12 @@ public class Rotation extends Conductor {
 
     private ImageButton btn_rotate90;
     private Button btn_reset;
+    private SeekBar angleSeekBar;
 
     private ImageView imageView;
     private MainActivity activity;
+
+    private TextView textViewAngle;
 
     private int currentAngle = 0;
 
@@ -49,9 +55,40 @@ public class Rotation extends Conductor {
         btn_rotate90 = activity.findViewById(R.id.rotate90);
         btn_reset = activity.findViewById(R.id.reset_seekbar);
 
+        textViewAngle = activity.findViewById(R.id.text_view_angle);
+
+        angleSeekBar = activity.findViewById(R.id.seekBar);
+        angleSeekBar.setProgress(90);
+        angleSeekBar.setMax(180);
+
         configRotate90Button(btn_rotate90);
         configResetButton(btn_reset);
-        // TODO: add seekBar
+
+        // Initialize the textview with '0'.
+        textViewAngle.setText("Current angle: " + (angleSeekBar.getProgress()-90));
+
+        angleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                Toast.makeText(activity.getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(activity.getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textViewAngle.setText("Current angle: " + ((angleSeekBar.getProgress()+progress-180)/2));
+                Toast.makeText(activity.getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+                System.out.println(progress);
+                rotateOnAngle(progress-90);
+            }
+        });
         
         bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
 
@@ -64,6 +101,9 @@ public class Rotation extends Conductor {
             @Override
             public void onClick(View v) {
                 imageView.setImageBitmap(bitmap);
+                currentAngle = 0;
+                angleSeekBar.setProgress(90);
+                textViewAngle.setText("Current angle: " + (angleSeekBar.getProgress()-90));
             }
         });
     }
@@ -74,12 +114,14 @@ public class Rotation extends Conductor {
             public void onClick(View v) {
                 rotateOnAngle(currentAngle + 90);
                 currentAngle += 90;
+                textViewAngle.setText("Current angle: " + currentAngle);
             }
         });
     }
 
     @SuppressLint("Assert")
     private void rotateOnAngle(int angle) {
+        if (angle < 0) angle += 360;
         int x = bitmap.getWidth();
         int y = bitmap.getHeight();
         double a = (double) (90 - angle % 90) * Math.PI / 180.0;
@@ -90,9 +132,9 @@ public class Rotation extends Conductor {
         Bitmap btmp;
         btmp = Bitmap.createBitmap((int) AB + 2, (int) AD + 2, Bitmap.Config.ARGB_8888);
         btmp = btmp.copy(Bitmap.Config.ARGB_8888, true);
-        Log.i("UPD", "hi");
+//        Log.i("UPD", "hi");
         for (int nx = 0; nx <= (int) AB; nx++) {
-            Log.i("UPD", ((Integer)nx).toString());
+//            Log.i("UPD", ((Integer)nx).toString());
             for (int ny = 0; ny <= (int) AD; ny++) {
                 int w = (int) (nx * sina - ny * cosa + x * cosa * cosa);
                 int h = (int) (nx * cosa + ny * sina - x * sina * cosa);
@@ -110,7 +152,7 @@ public class Rotation extends Conductor {
                 btmp.setPixel(nx, ny, bitmap.getPixel(w, h));
             }
         }
-        Log.i("UPD", "end");
+//        Log.i("UPD", "end");
 
         imageView.setImageBitmap(btmp);
         imageView.invalidate();
