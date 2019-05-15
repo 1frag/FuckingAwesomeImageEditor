266b51abf1dbe1c25dbaccbe24bda3ff1194bfe5
Aleksey Piskunov <piskunov.alesha@gmail.com>
2019-05-15 17:26:55 +0700

    usm - ready

--- app/src/main/java/com/example/image_editor/Usm.java
+++ app/src/main/java/com/example/image_editor/Usm.java
@@ -30,8 +30,7 @@ public class Usm extends Conductor {
 
     @Override
     void touchToolbar() {
-        // btn1 -> draw point
-        // btn2 -> do interpolation
+
         super.touchToolbar();
         prepareToRun(R.layout.sharpness_menu);
         setHeader("Unsharp masking");
@@ -192,83 +191,17 @@ public class Usm extends Conductor {
 
     // TODO: rewrite it to RGB
     private void algorithm() {
-        Log.i("upd", ((Integer) bitmap.getWidth()).toString() + " x " +
-                ((Integer) bitmap.getHeight()).toString());
 
-//        Bitmap blurred = (new GaussianBlur(mBitmap, mRadius)).algorithm();
         Bitmap blurred = ColorFIltersCollection.fastBlur(bitmap, (int) mRadius, 1);
-
-//        for (int w = 0; w < blurred.getWidth(); w++) {
-//            for (int h = 0; h < blurred.getHeight(); h++) {
-//                int now = mBitmap.getPixel(w, h);
-//                int R = (int) ((255 - Color.red(now)) / mAmount);
-//                int G = (int) ((255 - Color.green(now)) / mAmount);
-//                int B = (int) ((255 - Color.blue(now)) / mAmount);
-//                blurred.setPixel(w, h,
-//                        Color.rgb(R, G, B));
-//            }
-//        }
-//
-//        Bitmap unsharpMask = difference(mBitmap, blurred);
-//        Bitmap highContrast = increaseContrast(mBitmap, mAmount);
 
         for (int w = 0; w < bitmap.getWidth(); w++) {
             for (int h = 0; h < bitmap.getHeight(); h++) {
                 int origColor = bitmap.getPixel(w, h);
                 int blurColor = blurred.getPixel(w, h);
-//                int contrastColor = highContrast.getPixel(w, h);
 
                 bitmap.setPixel(w, h, fixColor(origColor, blurColor));
             }
         }
-    }
-
-    private double luminanceAsPercent(int pixel) {
-        int R = Color.red(pixel);
-        int G = Color.green(pixel);
-        int B = Color.blue(pixel);
-        return ((0.2126 * R) + (0.7152 * G) + (0.0722 * B)) / 255.0;
-    }
-
-    private Bitmap increaseContrast(Bitmap bitmap, double amount) {
-        Bitmap result = bitmap.copy(Bitmap.Config.RGB_565, true);
-        double F = (259.0 * (amount + 255)) / (255.0 * (259 - amount));
-        for (int i = 0; i < bitmap.getWidth(); i++) {
-            for (int j = 0; j < bitmap.getHeight(); j++) {
-                int now = bitmap.getPixel(i, j);
-                int R = Color.red(now);
-                R = (int) (F * (R - 128) + 128);
-                int G = Color.green(now);
-                G = (int) (F * (G - 128) + 128);
-                int B = Color.blue(now);
-                B = (int) (F * (B - 128) + 128);
-                if(R>255)R=255;if(R<0)R=0;
-                if(G>255)G=255;if(G<0)G=0;
-                if(B>255)B=255;if(B<0)B=0;
-                if (R>255 || B>255 || G>255 || R<0 || B<0 || G<0){
-                    Log.i("upd", "xepnZ");
-                    Log.i("upd", ((Integer)now).toString());
-                    Log.i("upd", ((Integer)R).toString());
-                    Log.i("upd", ((Integer)G).toString());
-                    Log.i("upd", ((Integer)B).toString());
-                    Log.i("upd", "xepnZ");
-                }
-                result.setPixel(i, j,
-                        Color.rgb(R, G, B));
-            }
-        }
-        return result;
-    }
-
-    private Bitmap difference(Bitmap bitmap, Bitmap blurred) {
-        Bitmap result = bitmap.copy(Bitmap.Config.RGB_565, true);
-        for (int i = 0; i < bitmap.getWidth(); i++) {
-            for (int j = 0; j < bitmap.getHeight(); j++) {
-                result.setPixel(i, j,
-                        bitmap.getPixel(i, j) - blurred.getPixel(i, j));
-            }
-        }
-        return result;
     }
 
 }
